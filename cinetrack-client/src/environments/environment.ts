aeedc7dbf9828690b9be45f8367270179a5ffa72
Daniel304F <Daniel-Fischer304@tutanota.de>
2025-03-25 22:02:35 +0100

    Fixed angular cli bug, added environments

---
+++ cinetrack-client/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyAE24QdtkOZXmxo1VoHjhRYQS171zxGlfQ",
+        authDomain: "cinetrack-9a47c.firebaseapp.com",
+        projectId: "cinetrack-9a47c",
+        storageBucket: "cinetrack-9a47c.firebasestorage.app",
+        messagingSenderId: "771747034902",
+        appId: "1:771747034902:web:711300845555e926178237",
+        measurementId: "G-HZ31NMM2TT"
+    }
+};
